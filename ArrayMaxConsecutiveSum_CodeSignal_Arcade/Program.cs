04874fb28fbf9031aa9ccbc8865121e7259b9d98
Albert Hakobyan <abhakobyan24@gmail.com>
2021-09-28 11:57:11 +0400

    Добавьте файлы проекта.

---
+++ ArrayMaxConsecutiveSum_CodeSignal_Arcade/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayMaxConsecutiveSum_CodeSignal_Arcade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = { 2, 3, 5, 1, 6 };
+            int k = 2;
+            int n = arrayMaxConsecutiveSum(inputArray,k);
+            Console.WriteLine(n);
+
+        }
+
+        static int arrayMaxConsecutiveSum(int[] inputArray, int k)
+        {
+            int max = 0;
+
+            for (int i = 0; i < inputArray.Length - (k - 1); i++)
+            {
+                int curr = 0;
+
+                for (int j = i; j < (i + k); j++)
+                {
+                    curr += inputArray[j];
+                }
+
+                if (curr > max) max = curr;
+            }
+
+            return max;
+        }
+    }
+}
